0503bab2ba2f43d5930f57b14bcd38ef929dcd97
vyerem <V1084fiat!>
2022-01-12 13:47:04 +0200

    Home task in progress 2

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -6,6 +6,61 @@ public class Task03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // TODO: Write your code here
+        System.out.print("Write how many ml of water the coffee machine has:\n >>> ");
+        String ingredientwater = scanner.nextLine();
+
+        System.out.print("Write how many ml of milk the coffee machine has:\n >>> ");
+        String ingredientmilk = scanner.nextLine();
+
+        System.out.print("Write how many grams of coffee beans the coffee machine has:\n >>> ");
+        String ingredientbeans = scanner.nextLine();
+
+        System.out.print("Write how many cups of coffee you will need: \n >>> ");
+        String cups = scanner.nextLine();
+
+        int usercups;
+        usercups = Integer.parseInt(cups);
+
+        int userwater;
+        userwater = Integer.parseInt(ingredientwater);
+
+        int usermilk;
+        usermilk = Integer.parseInt(ingredientmilk);
+
+        int userbeans;
+        userbeans = Integer.parseInt(ingredientbeans);
+
+        int water;
+        water = 200;
+        //System.out.print(water * usercups + " ml of water\n");
+
+        int milk;
+        milk = 50;
+        //System.out.print(milk * usercups + " ml of milk\n");
+
+        int coffeebeans;
+        coffeebeans = 15;
+        //System.out.print(coffeebeans * usercups + " g of coffee beans");
+
+        int leftwater;
+        leftwater = (userwater - water) / water;
+        int leftmilk;
+        leftmilk = (usermilk - milk) / milk;
+        int leftbeans;
+        leftbeans = (userbeans - coffeebeans) / coffeebeans;
+
+        int availablecups;
+        availablecups = Math.min(leftwater, Math.min(leftmilk,leftbeans));
+
+
+        if (availablecups == usercups) {
+            System.out.print("Yes, I can make that amount of coffee\n");
+        }  else if (userwater < water || usermilk < milk || userbeans < coffeebeans) {
+           System.out.print("No, I can make only " + availablecups + " cup(s) of coffee\n");
+       } else {
+           System.out.print("Yes, I can make that amount of coffee (and even " + availablecups + " more than that)\n");
+       }
+
 
     }
 }
